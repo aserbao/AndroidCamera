665d8d1c5bcc58c07c2aa91f9e7a8d28220e0f23
aserbao <1142803753@qq.com>
2018-05-04 17:41:38 +0800

    initialization

---
+++ app/src/main/java/com/aserbao/androidcustomcamera/blocks/BlocksActivity.java
@@ -0,0 +1,13 @@
+package com.aserbao.androidcustomcamera.blocks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlocksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocks);
+    }
+}
