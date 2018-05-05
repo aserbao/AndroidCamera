91c4586cc16eee638754dc7156bdca2acce7a984
aserbao <1142803753@qq.com>
2018-05-05 14:57:00 +0800

    添加查看和删除功能

--- app/src/main/java/com/aserbao/androidcustomcamera/blocks/mediacodec/primary/PrimaryMediaCodecActivity.java
+++ app/src/main/java/com/aserbao/androidcustomcamera/blocks/mediacodec/primary/PrimaryMediaCodecActivity.java
@@ -1,20 +1,23 @@
 package com.aserbao.androidcustomcamera.blocks.mediacodec.primary;
 
-import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
 import android.media.MediaFormat;
 import android.media.MediaMuxer;
+import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Surface;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.aserbao.androidcustomcamera.R;
 import com.aserbao.androidcustomcamera.base.activity.BaseActivity;
@@ -25,6 +28,7 @@ import java.lang.ref.WeakReference;
 import java.nio.ByteBuffer;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class PrimaryMediaCodecActivity extends BaseActivity {
@@ -36,16 +40,20 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
     private static final int FRAMES_PER_SECOND = 4;
     private static final int IFRAME_INTERVAL = 5;
 
-    private static final int NUM_FRAMES = 4 * 100 ;
+    private static final int NUM_FRAMES = 4 * 100;
     private static final int START_RECORDING = 0;
     private static final int STOP_RECORDING = 1;
 
     @BindView(R.id.btn_recording)
     Button mBtnRecording;
+    @BindView(R.id.btn_watch)
+    Button mBtnWatch;
     @BindView(R.id.primary_mc_tv)
     TextView mPrimaryMcTv;
     public MediaCodec.BufferInfo mBufferInfo;
     public MediaCodec mEncoder;
+    @BindView(R.id.primary_vv)
+    VideoView mPrimaryVv;
     private Surface mInputSurface;
     public MediaMuxer mMuxer;
     private boolean mMuxerStarted;
@@ -56,7 +64,49 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
     private int cuurFrame = 0;
 
     private MyHanlder mMyHanlder = new MyHanlder(this);
-    private static class MyHanlder extends Handler{
+    public File mOutputFile;
+
+    @OnClick({R.id.btn_recording, R.id.btn_watch})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_recording:
+                if (mBtnRecording.getText().equals("开始录制")) {
+                    try {
+                        mOutputFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), System.currentTimeMillis() + ".mp4");
+                        startRecording(mOutputFile);
+                        mPrimaryMcTv.setText("文件保存路径为：" + mOutputFile.toString());
+                        mBtnRecording.setText("停止录制");
+                        isRecording = true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        mBtnRecording.setText("出现异常了，请查明原因");
+                    }
+                } else if (mBtnRecording.getText().equals("停止录制")) {
+                    mBtnRecording.setText("开始录制");
+                    stopRecording();
+                }
+                break;
+            case R.id.btn_watch:
+                String absolutePath = mOutputFile.getAbsolutePath();
+                if (!TextUtils.isEmpty(absolutePath)) {
+                    if(mBtnWatch.getText().equals("查看视频")) {
+                        mBtnWatch.setText("删除视频");
+                        mPrimaryVv.setVideoPath(absolutePath);
+                        mPrimaryVv.start();
+                    }else if(mBtnWatch.getText().equals("删除视频")){
+                        if (mOutputFile.exists()){
+                            mOutputFile.delete();
+                            mBtnWatch.setText("查看视频");
+                        }
+                    }
+                }else{
+                    Toast.makeText(this, "请先录制", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private static class MyHanlder extends Handler {
         private WeakReference<PrimaryMediaCodecActivity> mPrimaryMediaCodecActivityWeakReference;
 
         public MyHanlder(PrimaryMediaCodecActivity activity) {
@@ -71,15 +121,15 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
                     case START_RECORDING:
                         activity.drainEncoder(false);
                         activity.generateFrame(activity.cuurFrame);
-                        Log.e(TAG, "handleMessage: " +activity.cuurFrame);
-                        if(activity.cuurFrame < NUM_FRAMES){
+                        Log.e(TAG, "handleMessage: " + activity.cuurFrame);
+                        if (activity.cuurFrame < NUM_FRAMES) {
                             this.sendEmptyMessage(START_RECORDING);
-                        }else{
+                        } else {
                             activity.drainEncoder(true);
                             activity.mBtnRecording.setText("开始录制");
                             activity.releaseEncoder();
                         }
-                        activity.cuurFrame ++;
+                        activity.cuurFrame++;
                         break;
                     case STOP_RECORDING:
                         Log.e(TAG, "handleMessage: STOP_RECORDING");
@@ -91,30 +141,11 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
             }
         }
     }
+
     @Override
     protected int setLayoutId() {
         return R.layout.activity_primary_media_codec;
     }
-
-    @OnClick(R.id.btn_recording)
-    public void onViewClicked() {
-        if(mBtnRecording.getText().equals("开始录制")){
-            try {
-                File outputFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), System.currentTimeMillis() + ".mp4");
-                startRecording(outputFile);
-                mPrimaryMcTv.setText("文件保存路径为："+ outputFile.toString());
-                mBtnRecording.setText("停止录制");
-                isRecording = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                mBtnRecording.setText("出现异常了，请查明原因");
-            }
-        }else if(mBtnRecording.getText().equals("停止录制")){
-            mBtnRecording.setText("开始录制");
-            stopRecording();
-        }
-    }
-
 
 
     private void startRecording(File outputFile) throws IOException {
@@ -127,6 +158,35 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
         mMyHanlder.removeMessages(START_RECORDING);
         mMyHanlder.sendEmptyMessage(STOP_RECORDING);
     }
+
+    /**
+     * 准备视频编码器，muxer，和一个输入表面。
+     */
+    private void prepareEncoder(File outputFile) throws IOException {
+        mBufferInfo = new MediaCodec.BufferInfo();
+        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, WIDTH, HEIGHT);
+
+        //1. 设置一些属性。没有指定其中的一些可能会导致MediaCodec.configure()调用抛出一个无用的异常。
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, BIT_RATE);//比特率(比特率越高，音视频质量越高，编码文件越大)
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAMES_PER_SECOND);//设置帧速
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);//设置关键帧间隔时间
+
+        //2.创建一个MediaCodec编码器，并配置格式。获取一个我们可以用于输入的表面，并将其封装到处理EGL工作的类中。
+        mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mInputSurface = mEncoder.createInputSurface();
+        mEncoder.start();
+        //3. 创建一个MediaMuxer。我们不能在这里添加视频跟踪和开始合成，因为我们的MediaFormat里面没有缓冲数据。
+        // 只有在编码器开始处理数据后才能从编码器获得这些数据。我们实际上对多路复用音频没有兴趣。我们只是想要
+        // 将从MediaCodec获得的原始H.264基本流转换为.mp4文件。
+        mMuxer = new MediaMuxer(outputFile.toString(), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+        mMuxerStarted = false;
+        mTrackIndex = -1;
+    }
+
     private void drainEncoder(boolean endOfStream) {
         final int TIMEOUT_USEC = 10000;
         if (endOfStream) {
@@ -140,10 +200,10 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
                 if (!endOfStream) {
                     break;      // out of while
                 }
-            }else if(encoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED){
+            } else if (encoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
                 //输出缓冲区已经更改，客户端必须引用新的
                 encoderOutputBuffers = mEncoder.getOutputBuffers();
-            }else if (encoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+            } else if (encoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
                 //输出格式发生了变化，后续数据将使用新的数据格式。
                 if (mMuxerStarted) {
                     throw new RuntimeException("format changed twice");
@@ -152,8 +212,8 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
                 mTrackIndex = mMuxer.addTrack(newFormat);
                 mMuxer.start();
                 mMuxerStarted = true;
-            }else if (encoderStatus < 0) {
-            }else{
+            } else if (encoderStatus < 0) {
+            } else {
                 ByteBuffer encodedData = encoderOutputBuffers[encoderStatus];
                 if (encodedData == null) {
                     throw new RuntimeException("encoderOutputBuffer " + encoderStatus +
@@ -167,6 +227,7 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
                     if (!mMuxerStarted) {
                         throw new RuntimeException("muxer hasn't started");
                     }
+                    //调整ByteBuffer值以匹配BufferInfo。
                     encodedData.position(mBufferInfo.offset);
                     encodedData.limit(mBufferInfo.offset + mBufferInfo.size);
                     mBufferInfo.presentationTimeUs = mFakePts;
@@ -189,35 +250,7 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
     }
 
     /**
-     * Prepares the video encoder, muxer, and an input surface.
-     */
-    private void prepareEncoder(File outputFile) throws IOException {
-        mBufferInfo = new MediaCodec.BufferInfo();
-        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, WIDTH, HEIGHT);
-
-        //1. 设置一些属性。没有指定其中的一些可能会导致MediaCodec.configure()调用抛出一个无用的异常。
-        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
-                MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
-        format.setInteger(MediaFormat.KEY_BIT_RATE, BIT_RATE);//比特率(比特率越高，音视频质量越高，编码文件越大)
-        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAMES_PER_SECOND);//设置帧速
-        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);//设置关键帧间隔时间
-
-        mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
-        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
-        mInputSurface = mEncoder.createInputSurface();
-        mEncoder.start();
-
-        mMuxer = new MediaMuxer(outputFile.toString(), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
-
-        mMuxerStarted = false;
-        mTrackIndex = -1;
-    }
-
-
-    /**
-     * Generates a frame, writing to the Surface via the "software" API (lock/unlock).
-     * <p>
-     * There's no way to set the time stamp.
+     * 生成一个框架，通过“软件”API(锁定/解锁)写入到表面。
      */
     private void generateFrame(int frameNum) {
         Canvas canvas = mInputSurface.lockCanvas(null);
@@ -238,25 +271,25 @@ public class PrimaryMediaCodecActivity extends BaseActivity {
                     color |= 0x000000ff;
                 }
                 paint.setColor(color);
-                canvas.drawRect(sliceWidth * i, 0, sliceWidth * (i+1), height, paint);
+                canvas.drawRect(sliceWidth * i, 0, sliceWidth * (i + 1), height, paint);
             }
 
             paint.setColor(0x80808080);
             float sliceHeight = height / 8;
             int frameMod = frameNum % 8;
-            canvas.drawRect(0, sliceHeight * frameMod, width, sliceHeight * (frameMod+1), paint);
+            canvas.drawRect(0, sliceHeight * frameMod, width, sliceHeight * (frameMod + 1), paint);
             paint.setTextSize(50);
             paint.setColor(0xffffffff);
 
-            canvas.drawText("aserbao",0,sliceHeight * frameMod,paint);
-            canvas.drawText("aserbao",1 * sliceWidth,sliceHeight * (frameMod+1),paint);
-            canvas.drawText("aserbao",2 * sliceWidth,sliceHeight * frameMod,paint);
-            canvas.drawText("aserbao",3 * sliceWidth,sliceHeight * (frameMod+1),paint);
-            canvas.drawText("aserbao",4 * sliceWidth,sliceHeight * frameMod,paint);
-            canvas.drawText("aserbao",5 * sliceWidth,sliceHeight * (frameMod+1),paint);
-            canvas.drawText("aserbao",6 * sliceWidth,sliceHeight * frameMod,paint);
-            canvas.drawText("aserbao",7 * sliceWidth,sliceHeight * (frameMod+1),paint);
-            canvas.drawText("aserbao",8 * sliceWidth,sliceHeight * frameMod,paint);
+            canvas.drawText("aserbao", 0, sliceHeight * frameMod, paint);
+            canvas.drawText("aserbao", 1 * sliceWidth, sliceHeight * (frameMod + 1), paint);
+            canvas.drawText("aserbao", 2 * sliceWidth, sliceHeight * frameMod, paint);
+            canvas.drawText("aserbao", 3 * sliceWidth, sliceHeight * (frameMod + 1), paint);
+            canvas.drawText("aserbao", 4 * sliceWidth, sliceHeight * frameMod, paint);
+            canvas.drawText("aserbao", 5 * sliceWidth, sliceHeight * (frameMod + 1), paint);
+            canvas.drawText("aserbao", 6 * sliceWidth, sliceHeight * frameMod, paint);
+            canvas.drawText("aserbao", 7 * sliceWidth, sliceHeight * (frameMod + 1), paint);
+            canvas.drawText("aserbao", 8 * sliceWidth, sliceHeight * frameMod, paint);
         } finally {
             mInputSurface.unlockCanvasAndPost(canvas);
         }
